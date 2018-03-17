7a7e40e081d1436faabcb50433a6fadb21cb09db
Jeffrey Meldgin <jmeldgin@gmail.com>
2018-03-17 16:05:20 -0700

    Got control basics working, tweaked Submarine, fixed submarine pivot point.

---
+++ Assets/Submarine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Submarine : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+        ProcessInput();
+	}
+    // Controls
+    private void ProcessInput()
+    {
+        // Thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        // Rotating Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        // Rotating Right
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
