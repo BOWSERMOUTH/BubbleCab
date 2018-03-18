82ba1522cfb55bfc78eb71b2aa8045869762e80a
Jeffrey Meldgin <jmeldgin@gmail.com>
2018-03-17 20:07:22 -0700

    GOT BUBBLES ON THRUST TO WORK

---
+++ Assets/BubblesOnThrust.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesOnThrust : MonoBehaviour {
+
+    ParticleSystem bubbleThrust;
+
+	// Use this for initialization
+	void Start ()
+    {
+        bubbleThrust = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        BubblesGo();
+    }
+
+    private void BubblesGo()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            bubbleThrust.enableEmission = true;
+        }
+        else
+        {
+            bubbleThrust.enableEmission = false;
+        }
+    }
+}
