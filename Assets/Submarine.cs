f224f7b80c4a3a19189f1fb8a5faa369a5c22abb
Jeffrey Meldgin <jmeldgin@gmail.com>
2018-03-17 18:33:34 -0700

    Added bubbles particle system, changed background and floor to image, added Down to controls

--- Assets/Submarine.cs
+++ Assets/Submarine.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Submarine : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	void Start ()
     {
         rigidBody = GetComponent<Rigidbody>();
-	}
+        audioSource = GetComponent<AudioSource>();
+    }
 	
 	void Update ()
     {
@@ -20,9 +22,23 @@ public class Submarine : MonoBehaviour {
     private void ProcessInput()
     {
         // Thrust
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            // So audio doesn't layer
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("going down");
+            rigidBody.AddRelativeForce(Vector3.down);
         }
         // Rotating Left
         if (Input.GetKey(KeyCode.A))
